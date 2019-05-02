c559dc59c8d1fbb2cbc433e258ec81fea107ad52
Vui Nguyen <vui_engineer@yahoo.com>
2019-05-02 10:16:55 -0600

    add a pin on map long press

--- VirtualTourist/Controller/TravelLocationsViewController.swift
+++ VirtualTourist/Controller/TravelLocationsViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
-class TravelLocationsViewController: UIViewController {
+class TravelLocationsViewController: UIViewController, MKMapViewDelegate {
+
+  @IBOutlet weak var mapView: MKMapView!
+
+  @IBAction func addPin(_ gestureRecognizer: UILongPressGestureRecognizer) {
+    //sender.minimumPressDuration
+    if gestureRecognizer.state == .ended {
+      print("I did a long press!")
+
+      // create annotation here
+      let touchPoint = gestureRecognizer.location(in: mapView)
+      let annotation = addAnnotation(touchPoint: touchPoint)
+      mapView.addAnnotation(annotation)
+    }
+  }
+
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  // persist zoom level and map center here
+  func setMapDefaults() {
+
+  }
+
+  func addAnnotation(touchPoint: CGPoint) -> MKPointAnnotation {
+    let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+    let annotation = MKPointAnnotation()
+    annotation.coordinate = newCoordinates
+    return annotation
+  }
 
 }
 
