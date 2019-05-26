c0fa82207eb5c4507bc1de76ed33bc13cf677749
Vui Nguyen <vui_engineer@yahoo.com>
2019-05-26 15:40:52 -0600

    setup before adding networking

---
+++ APIKeysExample.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeysExample.swift
+//  HelloFlickr
+//
+//  Created by Vui Nguyen on 5/26/19.
+//  Copyright © 2019 SunfishEmpire. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+  static let ApplicationID = "Add API Key for THIS Application HERE!"
+}
